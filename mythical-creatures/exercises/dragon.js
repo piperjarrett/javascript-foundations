83fc141a3d462fd3ac6cf1cee828940f57d584d0
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-07-12 15:34:59 -0600

    initial commit

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,20 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.amountEat = 0;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
+    this.amountEat = this.amountEat + 1;
 
+    if (this.amountEat === 3) {
+      this.hungry = false;
+    }
+  }
+}
+
+module.exports = Dragon;
