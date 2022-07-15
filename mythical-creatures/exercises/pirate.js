3bd113e27a555d40ceb9d45e309d935eafaddeb5
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-07-15 13:35:01 -0600

    add complete pirate

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,28 @@
+class Pirate {
+  constructor(name, job) {
+    this.name = name;
+    this.job = job || "scallywag";
+    this.cursed = false;
+    this.booty = 0;
+  }
+  robShip() {
+    if (this.booty >= 500) {
+      this.cursed = true;
+      return `ARG! I've been cursed!`;
+    }
+    if (this.booty < 500) {
+      this.booty = this.booty + 100;
+      return `YAARRR!`;
+    }
+  }
+  liftCurse() {
+    if (this.cursed == false) {
+      return `You don't need to lift a curse!`;
+    }
+    this.booty = this.booty - 300;
+    this.cursed = false;
+    return `Your curse has been lifted!`;
+  }
+}
+
+module.exports = Pirate;
