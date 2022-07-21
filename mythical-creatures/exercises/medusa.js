d23743f5af4670941010e85d6e3af10e564481f2
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-07-21 10:18:28 -0600

    add complete medusa and magician

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,23 @@
-var Person = require('./person');
-var Statue = require('./statue');
+var Person = require("./person");
+var Statue = require("./statue");
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim) {
+    var newStatue = new Statue(victim.name);
+    if (this.statues.length < 3) {
+      this.statues.push(newStatue);
+    } else {
+      var releasePerson = new Person(this.statues[0].name);
+      this.statues.shift();
+      this.statues.push(newStatue);
+      releasePerson.mood = "relieved";
+      return releasePerson;
+    }
+  }
+}
+
+module.exports = Medusa;
